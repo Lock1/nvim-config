2eaa95b4bd5efeaa9cd137212c27e98b7f0246af
Lock1 <30568743+Lock1@users.noreply.github.com>
2025-07-30 23:29:17 +0700

    feat: Autocompletion No Ghost

--- nvim/lua/plugins/auto-suggestion.lua
+++ nvim/lua/plugins/auto-suggestion.lua
@@ -1,14 +1,14 @@
 return {
-    "hrsh7th/nvim-cmp",
-    -- opts = function(_, opts)
-    --     local cmp = require("cmp")
-    --     opts.mapping = cmp.mapping({
-    --         ["<CR>"] = cmp.mapping.confirm({ select = false/true }),
-    --         ["<Esc>"] = cmp.mapping.abort(),
-    --         ["<Down>"] = cmp.mapping.select_next_item(),
-    --         ["<Up>"] = cmp.mapping.select_prev_item(),
-    --         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    --         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    --     })
-    -- end
+    "saghen/blink.cmp",
+    opts = {
+        completion = {
+            ghost_text = {
+                enabled = false,
+            }
+        },
+        keymap = {
+            preset = "default",
+            ["<Esc>"] = { "cancel" } -- First Esc during auto completion will reject suggestion while maintain insert mode
+        }
+    }
 }

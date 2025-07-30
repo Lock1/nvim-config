126987cd3308a5c9aac02d7807b945e9b2df99da
Lock1 <30568743+Lock1@users.noreply.github.com>
2025-07-31 00:16:53 +0700

    feat: Back to the basics

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,9 +1,20 @@
 return {
-    { "olimorris/onedarkpro.nvim" },
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "onedark_dark",
-        },
-    },
+    -- { "olimorris/onedarkpro.nvim" },
+    -- {
+    --     "LazyVim/LazyVim",
+    --     opts = {
+    --         colorscheme = "onedark_dark",
+    --     },
+    -- },
+    "gmr458/vscode_modern_theme.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("vscode_modern").setup({
+            cursorline = true,
+            transparent_background = false,
+            nvim_tree_darker = true,
+        })
+        vim.cmd.colorscheme("vscode_modern")
+    end,
 }
